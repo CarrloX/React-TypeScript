486bb473eda51bd8f77973a0abec44c3ad54768c
aquilessaltoasucama <aquilessaltoasucama@gmail.com>
2024-07-11 12:52:59 +0000

    f

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
-import { useState } from 'react'
+import { useState } from "react";
+import { Counter } from "./Componnents/Counter";
 
 function App() {
-
   return (
     <>
-      <h1>React+TypeScript</h1>
+      <h1>React+TypeScript </h1>
+      <hr />
+
+      <Counter/>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
